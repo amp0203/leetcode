dc0611f780fb2ff6ea6f74ae6877f77f91681466
msi\Phuong <abc@email>
2023-03-09 10:25:34 +0700

    no message

--- SumofAllOddLengthSubArrays.js
+++ SumofAllOddLengthSubArrays.js
@@ -17,10 +17,12 @@ var sumOddLengthSubarrays = function (arr) {
     let sumTotal = 0;
     for (let n = 1; n <= arr.length; n = n + 2) {
         for (let i = 0; i + n <= arr.length; i++) {
-            for (let s = i; s < i + n; s++) {
-                sumTotal += arr[s];
-            }
+            // sumTotal = arr
+            //     .slice(i, i + n)
+            //     .reduce((prev, total) => prev + total, 0);
         }
     }
     return sumTotal;
 };
+
+sumOddLengthSubarrays([1, 4, 2, 5, 3]);
