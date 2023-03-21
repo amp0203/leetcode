8c7233dd785b74cf0c521336d01bcfe6506bab4b
srph <srph@noreply.com>
2023-03-21 15:49:19 +0700

    Truncate sentence

--- TruncateSentence.js
+++ TruncateSentence.js
@@ -1,1 +1,7 @@
 //Truncate Sentence
+var truncateSentence = function (s, k) {
+    return s.split(' ').slice(0, k).join('');
+};
+
+truncateSentence('What is the solution to this problem', 4);
+//truncated
