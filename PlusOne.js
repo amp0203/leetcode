a1eaed4f406b0259daf2db2df716f7c941747ea6
srph <srph@noreply.com>
2023-05-04 21:28:36 +0700

    plus one

--- PlusOne.js
+++ PlusOne.js
@@ -1,4 +1,11 @@
 var plusOne = function (digits) {
-    return (+digits.join('') + 1).toString().split('').map(Number);
+    return (BigInt(digits.join('')) + 1n).toString().split('').map(Number);
 };
 plusOne([4, 5, 6]);
+
+var plusOne = function (digits) {
+    const number = +digits.join('');
+    const numOne = number + 1;
+    const arr = Array.from(String(numOne));
+    return arr;
+};
